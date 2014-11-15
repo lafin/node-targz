54a39f571cfffcba1983d5b95e03f131bdf42dbf
Kuvakin Sergey <kuvakin@gmail.com>
2014-11-16 01:31:08 +0300

    change module to better

--- example/example.js
+++ example/example.js
@@ -2,12 +2,14 @@ var tarGzip = require('../lib');
 
 tarGzip.compress({
     source: '../example',
-    destination: 'example.tar.gz',
+    destination: '../example.tar.gz',
     level: 6, // optional
     memLevel: 6 // optional
 }, function () {
     tarGzip.decompress({
-        source: 'example.tar.gz',
-        destination: './'
+        source: '../example.tar.gz',
+        destination: './unpack-example'
+    }, function () {
+        console.log('done');
     });
 });
